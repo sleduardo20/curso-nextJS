aa4b7ec3b04aabb00bec7e153d1d0f939592ffce
sleduardo20 <sl.eduardo20@gmail.com>
2020-09-30 22:14:54 -0300

    chore: config client side fatching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from 'react'
 import {Title} from '../styles/pages/Home'
 
+interface IProduct{
+  id: string;
+  title: string;
+}
+
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:3333/recommended').then(response =>{
+      response.json().then(data =>{
+        setRecommendedProducts(data);
+      })
+    })
+  },[])
+
   return (
     <div>
-      <Title>World hold on!</Title>
+      <section>
+      <Title>Products</Title>
+
+      <ul>
+        {recommendedProducts.map(recommendedProduct => (
+          <li key = {recommendedProduct.id}>
+              {recommendedProduct.title}
+          </li>
+        ))}
+      </ul>
+
+      </section>
     </div>  
   
   )
