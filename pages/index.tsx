31e8d7182c134bc48336bebc8ee04c2925d2049d
sleduardo20 <sl.eduardo20@gmail.com>
2020-09-30 21:55:06 -0300

    config styled-components

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import Head from 'next/head'
-
-
-export default function Home() {
-  return (
-    <div><h1>World hold on</h1></div>  
-  
-  )
-}
---
+++ src/pages/search.tsx
@@ -0,0 +1,6 @@
+export default function Search() {
+  return (
+    <div><h1>Search</h1></div>  
+  
+  )
+}
